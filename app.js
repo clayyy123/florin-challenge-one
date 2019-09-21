b325edf1f06f99fa4d3ac3be9e3a290bffdbcfb8
Clayton Wang <claydwang@gmail.com>
2019-09-21 15:02:25 -0700

    add sign in and sign up containers

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const signUpBtn = document.querySelector('.sign-up-overlay button');
 const signIn = document.querySelector('.sign-in');
 const signUp = document.querySelector('.sign-up');
 const signUpCont = document.querySelector('.sign-up-container');
+const signInCont = document.querySelector('.sign-in-container');
 const signInBtn = document.querySelector('.sign-in-overlay button');
 const overlayCon = document.querySelector('.overlay-container');
 const overlay = document.querySelector('.overlay');
@@ -9,9 +10,13 @@ const overlay = document.querySelector('.overlay');
 signInBtn.addEventListener('click', () => {
   overlay.classList.add('overlay-active');
   overlayCon.classList.add('overlay-container-active');
+  signUpCont.classList.add('sign-up-container-active');
+  signInCont.classList.remove('sign-in-container-active');
 });
 
 signUpBtn.addEventListener('click', () => {
   overlay.classList.remove('overlay-active');
   overlayCon.classList.remove('overlay-container-active');
+  signUpCont.classList.remove('sign-up-container-active');
+  signInCont.classList.add('sign-in-container-active');
 });
